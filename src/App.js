d8a1219cea892b332c9f69dfe587046ae2067610
chobuzz <139810780+chobuzz@users.noreply.github.com>
2023-12-13 21:20:05 +0900

    rockpaperscissor done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
 
@@ -8,30 +9,55 @@ import Box from "./component/Box";
 // 5. 박스의 색도 달라진다.
 
 const choice = {
-  rock: {
+  Rock: {
     name: "Rock",
     img: "https://substackcdn.com/image/fetch/w_1200,h_600,c_fill,f_jpg,q_auto:good,fl_progressive:steep,g_auto/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F3bdb2575-9a92-42f8-8472-bb78c7bd118a_720x405.jpeg",
   },
-  paper: {
+  Paper: {
     name: "Paper",
     img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg",
   },
-  scissor: {
+  Scissor: {
     name: "Scissor",
     img: "https://media.istockphoto.com/id/1146263300/vector/scissors-vector-design-illustration-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=mwp4-CXaKtvIq9VLvJbFtt94KhWmBPXJC9fFgrcPi3k=",
   },
 };
 
 function App() {
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+
   const play = (playerChoice) => {
-    console.log("선택됨!", playerChoice);
+    setUserChoice(choice[playerChoice]);
+    let computerChoice = randomchoice();
+    setComputerSelect(computerChoice);
+    setResult(outcome(choice[playerChoice], computerChoice));
+  };
+
+  const outcome = (user, computer) => {
+    console.log("user", user, "computer", computer);
+    if (user.name == computer.name) return "Tie";
+    else if (user.name == "Rock")
+      return computer.name == "Scissor" ? "Win" : "Lose";
+    else if (user.name == "Paper")
+      return computer.name == "Scissor" ? "Lose" : "Win";
+    else if (user.name == "Scissor")
+      return computer.name == "Paper" ? "Win" : "Lose";
+  };
+
+  const randomchoice = () => {
+    let itemArray = Object.keys(choice);
+    let random = ~~(Math.random() * itemArray.length);
+    let result = itemArray[random];
+    return choice[result];
   };
 
   return (
     <div>
       <div className="main">
-        <Box title="You" />
-        <Box title="Computer" />
+        <Box title="You" item={userChoice} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
 
       <div className="main">
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 const Box = (props) => {
+  let result;
+  if (
+    props.title == "Computer" &&
+    props.result != "Tie" &&
+    props.result != ""
+  ) {
+    result = props.result == "Win" ? "Lose" : "Win";
+  } else {
+    result = props.result;
+  }
   return (
-    <div className="box">
-      <h1 className="box-text">{props.title}</h1>
-      <img
-        className="item-img"
-        src="https://media.istockphoto.com/id/1146263300/vector/scissors-vector-design-illustration-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=mwp4-CXaKtvIq9VLvJbFtt94KhWmBPXJC9fFgrcPi3k="
-      ></img>
-      <h2 className="box-text">WIN</h2>
+    <div className={`box ${result}`}>
+      <h1 className="box-title">{props.title}</h1>
+      <img className="item-img" src={props.item && props.item.img}></img>
+      <h2 className="box-text">{result}</h2>
     </div>
   );
 };
